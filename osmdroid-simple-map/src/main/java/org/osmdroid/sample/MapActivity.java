b100e0157894d996cab8710a7f30ab7ac0002506
Khokhlinvladimir <82379382+Khokhlinvladimir@users.noreply.github.com>
2024-01-10 14:26:57 +0300

    Refactor MapActivity

--- osmdroid-simple-map/src/main/java/org/osmdroid/sample/MapActivity.java
+++ osmdroid-simple-map/src/main/java/org/osmdroid/sample/MapActivity.java
@@ -2,19 +2,11 @@ package org.osmdroid.sample;
 
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-
 import org.osmdroid.config.Configuration;
 import org.osmdroid.simplemap.R;
 import org.osmdroid.views.MapView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-/**
- * Bare bones osmdroid example
- * created on 2/17/2018.
- *
- * @author Alex O'Ree
- */
 
 public class MapActivity extends  android.app.Activity {
     private MapView mapView = null;
@@ -24,6 +16,7 @@ public class MapActivity extends  android.app.Activity {
         super.onCreate(savedInstance);
         Configuration.getInstance().load(getApplicationContext(),
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+
         //TODO check permissions
         setContentView(R.layout.activity_main);
         mapView = findViewById(R.id.mapView);
@@ -36,6 +29,9 @@ public class MapActivity extends  android.app.Activity {
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         if (mapView != null) {
             mapView.onResume();
+            mapView.setMultiTouchControls(true);
+            mapView.setMapOrientation(0, true);
+
         }
     }
 
