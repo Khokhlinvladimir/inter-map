0c30bb92ce48c76b25ae78f5dcc835f1c349738a
Khokhlinvladimir <82379382+Khokhlinvladimir@users.noreply.github.com>
2024-01-17 16:42:39 +0300

    Added request permission and find user location

--- osmdroid-simple-map/src/main/java/org/osmdroid/sample/MapActivity.kt
+++ osmdroid-simple-map/src/main/java/org/osmdroid/sample/MapActivity.kt
@@ -1,23 +1,37 @@
 package org.osmdroid.sample
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.osmdroid.config.Configuration.instance
 import org.osmdroid.simplemap.R
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.gestures.OneFingerZoomOverlay
 import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 
 class MapActivity : AppCompatActivity() {
     private var mapView: MapView? = null
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        }
+    }
+
     public override fun onCreate(savedInstance: Bundle?) {
         super.onCreate(savedInstance)
         instance!!.load(applicationContext,
                 PreferenceManager.getDefaultSharedPreferences(applicationContext))
 
-        //TODO check permissions
+        requestLocationPermission()
+
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
 
@@ -25,7 +39,6 @@ class MapActivity : AppCompatActivity() {
                 PreferenceManager.getDefaultSharedPreferences(applicationContext))
         if (mapView != null) {
             mapView!!.setTilesScaledToDpi(true)
-            // mapView!!.onResume()
             mapView!!.setMultiTouchControls(true)
             mapView!!.setMapOrientation(0f, true)
         }
@@ -35,10 +48,12 @@ class MapActivity : AppCompatActivity() {
         mapView!!.getOverlays()!!.add(mRotationGestureOverlay)
 
         //support for one finger zoom
-
-        //support for one finger zoom
         val mOneFingerZoomOverlay = OneFingerZoomOverlay()
         mapView!!.getOverlays()!!.add(mOneFingerZoomOverlay)
+
+        val mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), mapView)
+        mLocationOverlay.enableMyLocation()
+        mapView!!.getOverlays()!!.add(mLocationOverlay)
     }
 
     public override fun onResume() {
@@ -55,5 +70,25 @@ class MapActivity : AppCompatActivity() {
         if (mapView != null) {
             mapView!!.onPause()
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                //My Location
+
+
+
+            } else {
+                // Разрешение не было предоставлено
+            }
+        }
+    }
+
+
+    companion object {
+        const val REQUEST_LOCATION_PERMISSION = 1
     }
 }
