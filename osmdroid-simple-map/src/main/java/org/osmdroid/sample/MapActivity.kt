1171c032540705142e138a4ff13f7dec41d01be3
Khokhlinvladimir <82379382+Khokhlinvladimir@users.noreply.github.com>
2024-01-17 14:52:37 +0300

    Refactor MapActivity

--- osmdroid-simple-map/src/main/java/org/osmdroid/sample/MapActivity.kt
+++ osmdroid-simple-map/src/main/java/org/osmdroid/sample/MapActivity.kt
@@ -1,11 +1,19 @@
 package org.osmdroid.sample
 
+import android.graphics.Color
 import android.os.Bundle
 import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import org.osmdroid.config.Configuration.instance
 import org.osmdroid.simplemap.R
+import org.osmdroid.tileprovider.MapTileProviderBasic
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.TilesOverlay
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+
+
+
 
 class MapActivity : AppCompatActivity() {
     private var mapView: MapView? = null
@@ -17,16 +25,29 @@ class MapActivity : AppCompatActivity() {
         //TODO check permissions
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
+
+        instance!!.load(applicationContext,
+                PreferenceManager.getDefaultSharedPreferences(applicationContext))
+        if (mapView != null) {
+            mapView!!.setTilesScaledToDpi(true)
+            // mapView!!.onResume()
+            mapView!!.setMultiTouchControls(true)
+            mapView!!.setMapOrientation(0f, true)
+        }
+
+        val mRotationGestureOverlay = RotationGestureOverlay(mapView)
+        mRotationGestureOverlay.isEnabled = true
+        mapView!!.getOverlays()!!.add(mRotationGestureOverlay)
+
+        // Add tiles layer
+
+
     }
 
     public override fun onResume() {
         super.onResume()
-        instance!!.load(applicationContext,
-                PreferenceManager.getDefaultSharedPreferences(applicationContext))
         if (mapView != null) {
             mapView!!.onResume()
-            mapView!!.setMultiTouchControls(true)
-            mapView!!.setMapOrientation(0f, true)
         }
     }
 
